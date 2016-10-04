fd1299bdbec37a912440b0342522da2b67daedc3
VanTaS15 <vantas15@hotmail.com>
2016-10-04 15:23:15 +0300

    Add files via upload

---
+++ VoiceRecognitionRobotArm/VoiceRecognitionRobotArm/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace VoiceRecognitionRobotArm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(303, 224);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Speak Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Start Rec";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(240, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(159, 262);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Terminal";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(132, 316);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(193, 16);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Mike Mavroforakis & Vaggelis Tzavlakis";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(12, 313);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(56, 19);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Project by:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(328, 333);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "RoboArmBeta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ VoiceRecognitionRobotArm/VoiceRecognitionRobotArm/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace VoiceRecognitionRobotArm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pBuilder.ClearContent();
+            pBuilder.AppendText(textBox1.Text);
+            sSynth.Speak(pBuilder);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+
+
+            button2.Enabled = false;
+            button3.Enabled = true;
+            Choices sList = new Choices();
+            sList.Add(new string[] { "up", "UP", "down", "DOWN", "OFF", "off", "left", "LEFT","RIGHT", "right", "grap", "let", "exit", "default" });
+            Grammar gr = new Grammar(new GrammarBuilder(sList));
+            try
+            {
+                sRecognize.RequestRecognizerUpdate();
+                sRecognize.LoadGrammar(gr);
+                sRecognize.SpeechRecognized += sRecognize_SpeechRecognized;
+                sRecognize.SetInputToDefaultAudioDevice();
+                sRecognize.RecognizeAsync(RecognizeMode.Multiple);
+                
+                //Thread.Sleep(1000);
+                //SendKeys.SendWait("^+d");
+                //Thread.Sleep(1000);
+                //SendKeys.SendWait("^+c");
+                //Thread.Sleep(1000);
+                //SendKeys.SendWait("{ENTER}");
+                //Thread.Sleep(1000);
+                //SendKeys.SendWait("^+t");
+                //Thread.Sleep(1000);
+                // SendKeys.SendWait("LED.hex");
+                // Thread.Sleep(1000);
+                //SendKeys.SendWait("{ENTER}");
+                //Thread.Sleep(4000);
+
+            }
+
+            catch
+            {
+
+                return;
+            }
+        }
+
+        private void sRecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "exit":
+                    { textBox1.Text = textBox1.Text + "Exiting..";
+                        Application.Exit();
+                    }
+                    break;
+                case "up": case "UP": 
+                    {
+                        textBox1.Text = textBox1.Text + "\r\n" + e.Result.Text.ToString();
+
+                        SendKeys.SendWait("go 2");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    break;
+                case "down": case "DOWN":
+                    {
+                        textBox1.Text = textBox1.Text + "\r\n" + e.Result.Text.ToString();
+
+                        SendKeys.SendWait("go 1");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    break;
+                case "left": case "LEFT":
+                    {
+                        textBox1.Text = textBox1.Text + "\r\n" + e.Result.Text.ToString();
+
+                        SendKeys.SendWait("go 3");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                    }break;
+                case "right":case "RIGHT":
+                
+                    {
+                        textBox1.Text = textBox1.Text + "\r\n" + e.Result.Text.ToString();
+
+                        SendKeys.SendWait("go 4");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    break;
+                case "DEFAULT":case "default":
+                
+                    {
+                        textBox1.Text = textBox1.Text + "\r\n" + e.Result.Text.ToString();
+                        SendKeys.SendWait("go 0");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(5000);
+                        SendKeys.SendWait("off");
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(1000);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    break;
+
+
+                default: textBox1.Text = textBox1.Text + "\r\n" + "Didn't catch that,repeat..";
+                    break;
+            }
+
+
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sRecognize.RecognizeAsyncStop();
+            textBox1.Clear();
+           
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process myProcess;
+            myProcess = Process.Start("RoboPlusTerminal.exe");
+        }
+    }
+}
